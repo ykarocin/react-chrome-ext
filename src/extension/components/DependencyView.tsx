bcef460833173f412a11c2288e87b3aa0ce2dd4b
ykarocin <ysa@cin.ufpe.br>
2025-04-14 18:10:43 -0300

    Remoção de filtro de integer

--- src/extension/components/DependencyView.tsx
+++ src/extension/components/DependencyView.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import AnalysisService from "../../services/AnalysisService";
 import { dependency, modLine } from "../../models/AnalysisOutput";
-import { filterDuplicatedDependencies, updateLocationFromStackTrace, filterCFDependencies, filterCFSubStack } from "./dependencies";
+import { filterDuplicatedDependencies, updateLocationFromStackTrace,filterCFSubStack } from "./dependencies";
 import Conflict from "./Conflict";
 import DiffView from "./Diff/DiffView";
 import GraphView from "./Graph/GraphView";
@@ -277,7 +277,6 @@ export default function DependencyView({ owner, repository, pull_number }: Depen
             updateLocationFromStackTrace(dep, { inplace: true });
         });
         dependencies = filterDuplicatedDependencies(dependencies);
-        dependencies = filterCFDependencies(dependencies);
         dependencies = filterCFSubStack(dependencies);
 
         setDependencies(
